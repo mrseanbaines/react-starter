608372c899696a4ce749270128d7bf1baeeed5c4
Sean Baines <mrseanbaines@gmail.com>
2019-01-22 20:46:34 +0000

    Adjust configs etc.

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,22 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: ['babel-polyfill', './src'],
+  entry: ['babel-polyfill', path.resolve(__dirname, 'src')],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    contentBase: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+        loader: 'babel-loader',
+      },
+    ],
+  },
 };
