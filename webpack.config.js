054963a58cd6844c5b3f445ec9000e7da3d5e042
Sean Baines <mrseanbaines@gmail.com>
2019-06-27 08:20:43 +0200

    Add asset loaders

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,19 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
       },
     ],
   },
